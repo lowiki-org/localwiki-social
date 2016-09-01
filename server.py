b184f7f180a5bc6db4c5ca3fc7ffaf7f0a876feb
Superbil <superbil@gmail.com>
2016-09-02 00:19:21 +0800

    Let it can show view and upload limit

--- server.py
+++ server.py
@@ -1,34 +1,30 @@
-from flask import Flask, request, render_template
-from flask.ext.uploads import UploadSet
+from flask import Flask, request, render_template, flash
+from flask.ext.uploads import UploadSet, IMAGES, patch_request_class
 
-from twitter import *
-
+from twitter import Twitter, OAuth
 
 app = Flask(__name__)
-app.config.from_object('local.settings')
-photos = UploadSet(app.config['upload_folder'])
 
 
 @app.route("/", methods=['POST', 'GET'])
 def add():
-    error = None
-    if request.method == 'POST' and request.message:
-        request.form['']
-    return render_template('add.html', error=error)
+    return render_template('add.html')
 
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
-    if request.method == 'POST' and 'photo' in request.files:
+    error = None
+    photos = UploadSet(app.config['upload_folder'], IMAGES)
+    if request.method == 'POST' and request.message:
         filename = photos.save(request.files['photo'])
         rec = Photo(filename=filename, user=g.user.id)
         rec.store()
         flash("Photo saved.")
         return redirect(url_for('show', id=rec.id))
-    return render_template('upload.html')
+    return render_template('upload.html', error=error)
 
 
-def post_to_twitter(file_list):
+def post_to_twitter(message, file_list, coordinates=None):
     auth = OAuth(app.config['token'],
                  app.config['token_key'],
                  app.config['con_secret'],
@@ -45,8 +41,12 @@ def post_to_twitter(file_list):
                 image_id = t_upload.media.upload(media=image)["media_id_string"]
                 image_list.append(image_id)
 
+    # "geo": { "type":"Point", "coordinates":[37.78217, -122.40062] }
     t.statuses.update(status=message, media_ids=",".join(image_list))
 
 
 if __name__ == "__main__":
+    app.config.from_pyfile('localsettings.cfg')
+    patch_request_class(app, 5 * 1024 * 1024)
+
     app.run()
